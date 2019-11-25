e3d92e4994697a48465608e1f57b9f22cde975eb
vumg <ab17100222@gmail.com>
2019-11-24 21:35:34 -0600

    anagram ejercicio añadido

---
+++ anagram.js
@@ -0,0 +1,19 @@
+/*
+
+Un anagrama es una palabra o frase que resulta de la transposición de letras de otra palabra o frase.
+Una palabra es anagrama de otra si las dos tienen las mismas letras, con el mismo número de apariciones, pero en un orden diferente.
+
+*/
+
+function isAnagram(firstString,secondString){
+    let isAnagram = false;
+
+    if(firstString == secondString){
+        isAnagram = false;
+    }else if(firstString.split("").sort().join() == secondString.split("").sort().join()){
+        isAnagram = true;
+    }
+    return isAnagram;
+}
+
+console.log(isAnagram("desamparador","desparramado"));
