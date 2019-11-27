00d4fe9aa9e300de3e7140f011bd624873dd48f8
vumg <ab17100222@gmail.com>
2019-11-26 20:08:51 -0600

    chocolate cake ejercicio añadido

---
+++ chocolate.js
@@ -0,0 +1,27 @@
+// s = arreglo
+// d = day 
+// m = month
+
+function chocolate(s,d,m){
+
+    let contador = 0;
+
+    for(let i = 0; i<s.length - (m-1); i++){
+        let acumulador = 0;
+        for(let j = i; j<i+m; j++){
+            acumulador += s[j];
+        }
+        
+        if(acumulador == d ){
+             contador++;
+        }
+    }
+
+    return contador; 
+}
+
+let arr = [2, 3, 4, 4, 2, 1, 2, 5, 3, 4, 4, 3, 4, 1, 3, 5, 4, 5, 3, 1, 1, 5, 4, 3, 5, 3, 5, 3, 4, 4, 2, 4, 5, 2, 3, 2, 5, 3, 4, 2, 4, 3, 3, 4, 3, 5, 2, 5, 1, 3, 1, 4, 2, 2, 4, 3, 3, 3, 3, 4, 1, 1, 4, 3, 1, 5, 2, 5, 1, 3, 5, 4, 3, 3, 1, 5, 3, 3, 3, 4, 5, 2];
+let d = 26;
+let m = 8;
+
+console.log(chocolate(arr,d,m));
