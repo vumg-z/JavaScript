ccca31eb0fb39ff56cdd05851e70b1605e6c9768
vumg <ab17100222@gmail.com>
2020-02-05 16:43:42 -0600

    Deleted useless scripts

--- events_two.js
+++
@@ -1,5 +0,0 @@
-
-// Escucha para el evento.
-
-window.addEventListener('build', function (e) { alert("que haciendo") }, false);
-
--- hoisting.js
+++
@@ -1,9 +0,0 @@
-//  let var y const
-
-function scope(){
-    let myVariable = 4;
-    let myVariable = 3;
-    console.log(myVariable); //3
-}
-
-scope();
