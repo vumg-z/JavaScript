dad8aaed3116753d314642f377437db761b7c886
vumg <ab17100222@gmail.com>
2020-02-09 18:08:44 -0600

    Day of the programmer

---
+++ day_of_the_programmer.js
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+const dayOfProgrammer = year => {
+    
+  if (year > 1916 && year != 1918) {
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+      return "12.09." + year;
+    else return "13.09." + year;
+
+  } 
+  
+  else if (year == 1918) return "26.09." + year;
+  else {
+
+    if (year % 4 == 0) return "12.09." + year;
+    else return "13.09." + year;
+
+  }
+};
