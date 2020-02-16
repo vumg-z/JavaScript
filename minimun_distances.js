4ac6ed5a3a5a220e9e68bf747aee64602a897846
vumg <ab17100222@gmail.com>
2020-02-15 23:52:29 -0600

    Minimun Distances

---
+++ minimun_distances.js
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/minimum-distances/problem
+
+
+const minimumDistance = a => {
+    let map = new Map;
+    let exists = false;
+
+    for(let i = 0; i<a.length; i++){
+
+        let number = a[i];
+        if(!map.has(number)){
+            let firstPair = a.indexOf(number);
+            let secondPair = a.lastIndexOf(number)
+
+            if(firstPair != secondPair){
+                map.set(number, secondPair - firstPair)
+                exists = true;
+            }else{
+                map.set(number, 0)
+            }
+        }
+
+    }
+
+    if(exists){
+        let valuesArray = Array.from(map.values());
+        valuesArray.sort((a,b) => a-b)
+        let number = valuesArray.filter((num) => num > 0 )[0]
+        return number
+    }else{
+        return -1
+    }
+}
+
+
+let test1 = [3,2,1,2,3]
+
+console.log(minimumDistance(test1))
